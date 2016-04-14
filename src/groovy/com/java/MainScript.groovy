817339ca038c27a57bf0c43c35a679b63e6b54d0
ad3942soit <ad3942soit@gmail.com>
2016-04-14 14:33:06 +0300

    added script for gerrit

--- src/groovy/com/java/MainScript.groovy
+++ src/groovy/com/java/MainScript.groovy
@@ -70,6 +70,7 @@ if(linesNmbr!=0) {
             }
             environmentVariables {
                 env('gerritUrl',gerritUrl)
+                env('nameProject',nameProject)
             }
 
             triggers{
@@ -110,7 +111,7 @@ if(linesNmbr!=0) {
 |git config --global push.default simple
 |git remote add --mirror=push github ${gerritUrl}
 |git remote -v
-|git push ${gerritUrl}
+|git push ${gerritUrl} HEAD:refs/for/master
 |git clone ${gerritUrl} HEAD:refs/for/master
 |git branch -r
 |set -x'''.stripMargin())
